40741b78cff7560abb912778f0a553aa0170a134
Wesly <weslyramalho@yahoo.com.br>
2022-01-22 15:23:23 -0300

    Criado metodo para buscar alunos por nome

--- src/main/java/com/wr/cadastro/AlunosController.java
+++ src/main/java/com/wr/cadastro/AlunosController.java
@@ -2,6 +2,7 @@ package com.wr.cadastro;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,7 +27,12 @@ public class AlunosController {
 	}
 	
 	@GetMapping
-	public List<Alunos> alunos() {
+	public List<Alunos> findAll(@RequestParam(required = false) String alunos) {
+		if(alunos != null) {
+			return alunosList.stream()
+					.filter(al -> al.getNome().contains(alunos))
+					.collect(Collectors.toList());
+		}
 		return alunosList;
 	}
 	
