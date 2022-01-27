5ebd2a2c7c9d799c4a73ec121daabcc4293a0301
Wesly <weslyramalho@yahoo.com.br>
2022-01-27 16:29:17 -0300

    Criado cadastro de alunos em AlunosService

--- src/main/java/com/wr/cadastro/AlunosService.java
+++ src/main/java/com/wr/cadastro/AlunosService.java
@@ -1,5 +1,34 @@
 package com.wr.cadastro;
 
-public class AlunosService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+
+
+public class AlunosService {
+	
+	private List<Alunos> alunosList;
+	
+	public AlunosService() {
+		this.alunosList= new ArrayList<>();
+
+	}
+
+	public List<Alunos> BuscarAlunos(String alunos) {
+		
+		if(alunos != null) {
+			return alunosList.stream()
+					.filter(al -> al.getNome().contains(alunos))
+					.collect(Collectors.toList());
+		}
+		return alunosList;
+	}
+	
+	public Alunos AlunosPorID(Integer id) {
+		return this.alunosList.stream()
+				.filter(al -> al.getId().equals(id))
+				.findFirst()
+				.orElse(null);
+	}
 }
