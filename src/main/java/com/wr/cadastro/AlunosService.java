b14ef198cb9eeb122abd07098d17a65c5cf898d9
Wesly <weslyramalho@yahoo.com.br>
2022-01-27 16:36:49 -0300

    criado buscar por nome em Alunosservice

--- src/main/java/com/wr/cadastro/AlunosService.java
+++ src/main/java/com/wr/cadastro/AlunosService.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 public class AlunosService {
@@ -15,20 +20,21 @@ public class AlunosService {
 
 	}
 
-	public List<Alunos> BuscarAlunos(String alunos) {
+	public void addNovoAluno(@RequestBody Alunos alunos) {
+		
+		int id = alunosList.size() + 1;
+		alunos.setId(id);
+		alunosList.add(alunos);
+		
+		new ResponseEntity<>("Cadastrado com sucesso!", HttpStatus.CREATED);
+	}
+	public List<Alunos> findAllPorNome(@RequestParam(required = false) String alunos) {
 		
 		if(alunos != null) {
 			return alunosList.stream()
 					.filter(al -> al.getNome().contains(alunos))
 					.collect(Collectors.toList());
 		}
-		return alunosList;
-	}
-	
-	public Alunos AlunosPorID(Integer id) {
-		return this.alunosList.stream()
-				.filter(al -> al.getId().equals(id))
-				.findFirst()
-				.orElse(null);
+		throw new NomeNaoEncontradoExeception();
 	}
 }
---
+++ src/main/java/com/wr/cadastro/NomeNaoEncontradoExeception.java
@@ -0,0 +1,5 @@
+package com.wr.cadastro;
+
+public class NomeNaoEncontradoExeception extends Exception {
+
+}
